8cd4794c6b06763a342f66b53ec08494f0e3aeeb
Adarsh-s452 <adarshappuadhi@.com>
2025-02-06 16:12:07 +0530

    third

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import movieRoutes from './routes/movieRoutes.js';
 import cors from 'cors';
+import path from 'path';
+import { getMovieById } from './controllers/movieController.js';
 
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
@@ -17,10 +20,12 @@ mongoose.connect(process.env.MONGODB_URI)
 // Middleware
 app.use(cors());
 app.use(express.json()); // To parse JSON in requests
-app.use(express.static('uploads')); // Serve static files from the 'uploads' folder
-
+app.use('/uploads', express.static(path.join(path.resolve(), 'uploads'))); // Serve uploaded files
 // Routes
 app.use('/movies', movieRoutes);
+
+app.get('/movies/getMovieById/:id', getMovieById);  // Use the getMovieById controller for this route
+
 
 app.get('/', (req, res) => {
   res.send(`<h1>Backend server is running....</h1>`);
